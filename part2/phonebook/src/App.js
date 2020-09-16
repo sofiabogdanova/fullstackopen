556eab50c04328a487379473b4eba4d4fe3fa5ce
SOFIA <rusofiaru@gmail.com>
2020-09-16 18:17:01 +0300

    phonebook project - use server for fetching data

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import PersonForm from './components/personForm'
 import Persons from './components/persons'
 import SearchFilter from './components/searchFilter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filteredValue, setFilteredValue] = useState('');
@@ -53,6 +49,15 @@ const App = () => {
     setFilteredPersons(newFilteredPersons);
     setFilteredValue(filterValue);
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setFilteredPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
