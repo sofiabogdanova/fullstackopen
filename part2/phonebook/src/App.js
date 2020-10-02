71369c7ed775e999583ea1eea974b985b4fc365c
SOFIA <rusofiaru@gmail.com>
2020-10-02 13:39:17 +0300

    validation error catch

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -51,7 +51,13 @@ const App = () => {
         setTimeout(() => {
           setSuccessMessage(null)
         }, 5000)
-      });
+      })
+        .catch(error => {
+            setErrorMessage(error.response.data.error);
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        });
   }
 
   const handleNameChange = (event) => {
